cbfaedafacdb9ac626157376cb8cd4974b328432
beggers <be@spiegelburg.net>
2019-01-10 00:11:10 +0100

    Add naming unit tests

---
+++ src/Simplic.Tenant.UnitTest/NameTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simplic.Tenant.Data.Memory;
+using Simplic.Tenant.Service;
+using Unity;
+
+namespace Simplic.Tenant.UnitTest
+{
+    [TestClass]
+    public class NameTest
+    {
+        private IUnityContainer unityContainer;
+
+        public NameTest()
+        {
+            unityContainer = new UnityContainer();
+            unityContainer.RegisterType<IOrganizationTenantRepository, OrganizationTenantRepository>();
+            unityContainer.RegisterType<IOrganizationTenantService, OrganizationTenantService>();
+        }
+
+        [TestMethod]
+        public void NameOrderTest()
+        {
+            var tenants = new OrganizationTenant[]
+            {
+                new OrganizationTenant { Name = "Tenant 1", MatchCode = "T1" },
+                new OrganizationTenant { Name = "Tenant 2", MatchCode = "T2" },
+                new OrganizationTenant { Name = "Tenant 3", MatchCode = "T3" }
+            };
+
+            var service = unityContainer.Resolve<IOrganizationTenantService>();
+
+            var id = service.CreateOrGetGroup(tenants);
+            var group = service.Get(id);
+
+            Assert.AreEqual(group.Name, "Tenant 1;Tenant 2;Tenant 3");
+        }
+
+        [TestMethod]
+        public void MatchCodeOrderTest()
+        {
+            var tenants = new OrganizationTenant[]
+            {
+                new OrganizationTenant { Name = "Tenant 1", MatchCode = "T1" },
+                new OrganizationTenant { Name = "Tenant 2", MatchCode = "T2" },
+                new OrganizationTenant { Name = "Tenant 3", MatchCode = "T3" }
+            };
+
+            var service = unityContainer.Resolve<IOrganizationTenantService>();
+
+            var id = service.CreateOrGetGroup(tenants);
+            var group = service.Get(id);
+
+            Assert.AreEqual(group.MatchCode, "T1;T2;T3");
+        }
+    }
+}
