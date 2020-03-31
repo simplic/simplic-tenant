338fb01db267c7d7cebd04dc66211f756a81a2de
beggers <be@spiegelburg.net>
2020-03-31 23:25:33 +0200

    Update version and change nuget

--- src/Simplic.Studio.Tenant.UI/Properties/AssemblyInfo.cs
+++ src/Simplic.Studio.Tenant.UI/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.119.429")]
-[assembly: AssemblyFileVersion("1.3.119.429")]
+[assembly: AssemblyVersion("2.0.20.331")]
+[assembly: AssemblyFileVersion("2.0.20.331")]
